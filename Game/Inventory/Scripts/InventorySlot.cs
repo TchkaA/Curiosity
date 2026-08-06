51a0efe9734e8212cb304c145ee08848a3b354c6
TchkaA <Dangereydle@gmail.com>
2026-08-06 20:32:20 +0300

    fix: Исправить баг с заложеным инвентарем

--- Game/Inventory/Scripts/InventorySlot.cs
+++ Game/Inventory/Scripts/InventorySlot.cs
@@ -168,7 +168,7 @@ public partial class InventorySlot : Control, IContextMenuProvider
 
     private void Transfer()
     {
-        BookMenu.Instance.Transfer(Owner, CurrentItem.Item, CurrentItem.Count);
+        BookMenu.Instance.Transfer(GetParent<InventoryGrid>(), CurrentItem.Item, 1);
     }
 
 }
